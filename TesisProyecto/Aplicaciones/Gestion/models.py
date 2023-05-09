edb32262865180ab6739d3d4bc9b78211a519ba5
marcook888 <marcosrojas772@gmail.com>
2023-05-08 21:09:05 -0500

    ingreso productos vergas

--- TesisProyecto/Aplicaciones/Gestion/models.py
+++ TesisProyecto/Aplicaciones/Gestion/models.py
@@ -6,3 +6,7 @@ class Producto(models.Model):
     codigo=models.CharField(primary_key=True,max_length=6)
     nombre=models.CharField(max_length=50)
     Precio=models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        texto="#{0} {1} / precio: {2} $"
+        return texto.format(self.codigo,self.nombre, self.Precio)

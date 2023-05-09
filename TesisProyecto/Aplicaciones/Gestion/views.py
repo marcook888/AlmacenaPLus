9abf6732ded269c92371870e4e2b1d31e7974ba6
marcook888 <61944583+marcook888@users.noreply.github.com>
2023-05-09 15:03:34 -0500

    chan chin

--- TesisProyecto/Aplicaciones/Gestion/views.py
+++ TesisProyecto/Aplicaciones/Gestion/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def home(request):
-    return render(request, "gestionGestion.html")
+    ProductoListados = Producto.objects.all() 
+    return render(request, "gestionGestion.html", {"Producto": ProductoListados})

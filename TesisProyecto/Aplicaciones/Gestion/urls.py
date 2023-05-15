67cdfa72610edbc738a3a9c296d5d662093c26d1
marcook888 <marcosrojas772@gmail.com>
2023-05-15 00:17:35 -0500

    crud completo y bonito amen

--- TesisProyecto/Aplicaciones/Gestion/urls.py
+++ TesisProyecto/Aplicaciones/Gestion/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('', views.home),
-    path('registrarProducto/', views.registrarProducto)
+    path('registrarProducto/', views.registrarProducto),
+    path('edicionProducto/<codigo>', views.edicionProducto),
+    path('editarProducto/', views.editarProducto),
+    path('eliminacionProducto/<codigo>', views.eliminacionProducto)
+    
 ]
  
--- TesisProyecto/Aplicaciones/Gestion/views.py
+++ TesisProyecto/Aplicaciones/Gestion/views.py
@@ -14,3 +14,24 @@ def registrarProducto(request):
 
     producto=Producto.objects.create(codigo=codigo, nombre=nombre, precio=precio)
     return redirect('/')
+
+def edicionProducto (request, codigo):
+    producto=Producto.objects.get(codigo=codigo)
+    return render (request, "edicionProducto.html", {"producto":producto})
+
+def editarProducto(request):
+    codigo=request.POST['txtCodigo']
+    nombre=request.POST['txtNombre']
+    precio=request.POST['numPrecio']
+
+    producto=Producto.objects.get(codigo=codigo)
+    producto.nombre=nombre
+    producto.precio=precio
+    producto.save()
+
+    return redirect('/')
+
+def eliminacionProducto(request, codigo):
+    producto=Producto.objects.get(codigo=codigo)
+    producto.delete()
+    return redirect('/')

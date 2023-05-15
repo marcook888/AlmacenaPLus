8a032958137fde7317fd09b6c4e5517048059b1c
marcook888 <marcosrojas772@gmail.com>
2023-05-15 01:02:41 -0500

    falta cambiar a postgres pero ya tengo sueño

--- TesisProyecto/Aplicaciones/Gestion/views.py
+++ TesisProyecto/Aplicaciones/Gestion/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Producto
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     ProductoListados = Producto.objects.all() 
+    messages.success(request, '!Productos Listados!')
     return render(request, "gestionGestion.html", {"Producto": ProductoListados})
 
 def registrarProducto(request):
@@ -13,6 +15,7 @@ def registrarProducto(request):
     precio=request.POST['numPrecio']
 
     producto=Producto.objects.create(codigo=codigo, nombre=nombre, precio=precio)
+    messages.success(request, '!Producto Registrado!')
     return redirect('/')
 
 def edicionProducto (request, codigo):
@@ -28,10 +31,12 @@ def editarProducto(request):
     producto.nombre=nombre
     producto.precio=precio
     producto.save()
+    messages.success(request, '!Producto Actualizado!')
 
     return redirect('/')
 
 def eliminacionProducto(request, codigo):
     producto=Producto.objects.get(codigo=codigo)
     producto.delete()
+    messages.success(request, '!Producto Eliminado!')
     return redirect('/')
